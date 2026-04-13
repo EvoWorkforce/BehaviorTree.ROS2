3f885f794015241e3fc7dd84572da62c6a0ef646
Marcelo Couto <marcelo.couto@evoworkforce.com>
2026-04-13 16:09:46 +0100

    Change nodelay from 0 to 10 milliseconds

--- behaviortree_ros2/include/behaviortree_ros2/bt_action_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_action_node.hpp
@@ -461,7 +461,7 @@ inline NodeStatus RosActionNode<T>::tick()
     // FIRST case: check if the goal request has a timeout
     if(!goal_received_)
     {
-      auto nodelay = std::chrono::milliseconds(0);
+      auto nodelay = std::chrono::milliseconds(10);
       auto timeout =
           rclcpp::Duration::from_seconds(double(server_timeout_.count()) / 1000);
 
